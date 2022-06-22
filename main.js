a46e99e23cb0792534b4e1723630da694708a681
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-06-22 23:59:35 +0600

    feat: js logic

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
+const popup = document.querySelector(".popup");
+const popupForm = popup.querySelector(".popup__form");
+const inputName = popupForm.querySelector(".popup__input-name");
+const inputProf = popupForm.querySelector(".popup__input-prof");
+const profileName = document.querySelector(".profile__title");
+const profileProf = document.querySelector(".profile__profession");
+const openPopup = document.querySelector(".profile__button-edit");
+const closePopup = popup.querySelector(".popup__button-close");
+const likeBtn = document.querySelector(".gallery__button");
+
 //Открытие и закрытие формы//
 
-const openPopUp = document.querySelector(".profile__button-edit");
-const closePopUp = document.querySelector(".popup__button-close");
-const popUp = document.querySelector(".popup");
+function popupCloseHandler() {
+  popup.classList.remove("popup__active");
+}
 
-openPopUp.addEventListener("click", () => {
-  popUp.classList.add("active");
+openPopup.addEventListener("click", () => {
+  inputName.setAttribute("value", profileName.textContent);
+  inputProf.setAttribute("value", profileProf.textContent);
+  popup.classList.add("popup__active");
 });
 
-closePopUp.addEventListener("click", () => {
-  popUp.classList.remove("active");
+//Отправка формы//
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileProf.textContent = inputProf.value;
+  popupCloseHandler();
+}
+
+popupForm.addEventListener("submit", formSubmitHandler);
+closePopup.addEventListener("click", popupCloseHandler);
+
+likeBtn.addEventListener("click", () => {
+  likeBtn.classList.toggle("gallery__button_active");
 });
-
-// //Отправка формы//
-
-// // const popUpForm = document.querySelector("popup__form");
-
-// // const inputName = document.querySelector(".popup__input-name");
-// // const inputProf = document.querySelector(".popup__input-prof");
-
-// // function formSubmitHandler(evt) {
-// //   evt.preventDefault();
-// // }
-
-// // const profileName = document.querySelector(".profile__title");
-// // const profileProf = document.querySelector(".profile__profession");
-// // const nameText = inputName.value;
-// // const profText = inputProf.value;
-// // inputName.addEventListener("input", () => {
-// //   profileName.textContent = inputName.value;
-// // });
-
-// // inputProf.addEventListener("input", () => {
-// //   profileProf.textContent = inputProf.value;
-// });
-// // popUpForm.addEventListener("submit", formSubmitHandler);
