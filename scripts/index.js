6c5221bf39d6512d14aca4bb807d42ea65018606
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-06-29 17:21:38 +0600

    feat: add class active for all like-btns

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ const profileName = document.querySelector(".profile__title");
 const profileProf = document.querySelector(".profile__profession");
 const openPopup = document.querySelector(".profile__edit-button");
 const closePopup = popup.querySelector(".popup__close-button");
-const likeBtn = document.querySelector(".gallery__like-button");
+const likeBtn = document.querySelectorAll(".gallery__like-button");
 
 openPopup.addEventListener("click", () => {
   inputName.value = profileName.textContent;
@@ -29,6 +29,8 @@ function formSubmitHandler(event) {
 popupForm.addEventListener("submit", formSubmitHandler);
 closePopup.addEventListener("click", popupCloseHandler);
 
-likeBtn.addEventListener("click", () => {
-  likeBtn.classList.toggle("gallery__like-button_active");
+likeBtn.forEach((i) => {
+  i.addEventListener("click", () => {
+    i.classList.toggle("gallery__like-button_active");
+  });
 });
