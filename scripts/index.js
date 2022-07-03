230c63828830ea301b210e2a58bd8ff5c621064b
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-07-03 20:48:26 +0600

    feat: delete-button add

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,51 @@ const profileName = document.querySelector(".profile__title");
 const profileProf = document.querySelector(".profile__profession");
 const openPopup = document.querySelector(".profile__edit-button");
 const closePopup = popup.querySelector(".popup__close-button");
-const likeBtn = document.querySelectorAll(".gallery__like-button");
+const galleryList = document.querySelector(".gallery__list");
+
+const galleryTemplate = document.querySelector(".gallery__template").content;
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const createGalleryElement = () => {
+  const galleryElement = galleryTemplate
+    .querySelector(".gallery__list-item")
+    .cloneNode(true);
+
+  galleryElement.querySelector(".gallery__image").src = initialCards[0].link;
+  galleryElement.querySelector(".gallery__image").alt = initialCards[0].name;
+  galleryElement.querySelector(".gallery__title").textContent =
+    initialCards[0].name;
+
+  galleryList.prepend(galleryElement);
+};
+
+createGalleryElement();
 
 openPopup.addEventListener("click", () => {
   inputName.value = profileName.textContent;
@@ -28,9 +72,3 @@ function formSubmitHandler(event) {
 
 popupForm.addEventListener("submit", formSubmitHandler);
 closePopup.addEventListener("click", popupCloseHandler);
-
-likeBtn.forEach((i) => {
-  i.addEventListener("click", () => {
-    i.classList.toggle("gallery__like-button_active");
-  });
-});
