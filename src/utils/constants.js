008c247d2c1db8781ff3a2be1e79e44eb564263c
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-08-30 00:18:24 +0600

    fix: name for initial cards

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,7 @@ export const selectorClasses = {
 };
 
 export const classCreationSelectors = {
-  userName: ".profile__title",
+  userplace: ".profile__title",
   userJob: ".profile__profession",
   profilePopup: ".popup_profile",
   placePopup: ".popup_new-place",
@@ -19,27 +19,27 @@ export const classCreationSelectors = {
 
 export const initialCards = [
   {
-    name: "Dubai",
+    place: "Dubai",
     link: "https://sun9-2.userapi.com/impf/c836336/v836336516/2d38c/eIyNJ872gkE.jpg?size=2560x1440&quality=96&sign=20edbe892b946bc474d6d08d3e51fdbf&type=album",
   },
   {
-    name: "Челябинская область",
+    place: "Челябинская область",
     link: "https://sun9-35.userapi.com/impf/c540104/v540104823/9716/5326SEkyd_c.jpg?size=1920x1080&quality=96&sign=90a662783ce25d47b2bc15c45f273a1b&type=album",
   },
   {
-    name: "Lake",
+    place: "Lake",
     link: "https://sun9-18.userapi.com/impf/c834100/v834100519/9fe92/FLlPk19xabQ.jpg?size=2560x1440&quality=96&sign=599ed8e4d5e1e5ffc3555f554a4a649c&type=album",
   },
   {
-    name: "Black hole",
+    place: "Black hole",
     link: "https://sun9-30.userapi.com/impf/c851236/v851236254/fc608/jZDBy50vIEc.jpg?size=1920x1080&quality=96&sign=fbb545e2ca15c74c7cceb911c3e02387&type=album",
   },
   {
-    name: "Mountain",
+    place: "Mountain",
     link: "https://sun9-84.userapi.com/impf/c855120/v855120863/ce7b0/hNY44W1g5Ck.jpg?size=1920x1080&quality=96&sign=dbd8ebea7ae4aeb415cd7513b878459e&type=album",
   },
   {
-    name: "Hogwarts",
+    place: "Hogwarts",
     link: "https://sun9-41.userapi.com/impf/c854216/v854216045/d0800/O0cdrU-jt9I.jpg?size=1920x1080&quality=96&sign=085b0a512bf8a6cec15e338dcf323b23&type=album",
   },
 ].reverse();
