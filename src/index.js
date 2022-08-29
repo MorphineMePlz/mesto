1307f541bf4a63bcc8d8346edc844e432020678a
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-08-29 15:36:51 +0600

    feat: arrange webpack

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import "./pages/index.css";
+import Card from "./scripts/Card.js";
+import FormValidator from "./scripts/FormValidator.js";
+import Section from "./scripts/Section.js";
+import PopupWithImage from "./scripts/PopupWithImage.js";
+import PopupWithForm from "./scripts/PopupWithForm.js";
+import UserInfo from "./scripts/UserInfo.js";
 
 import {
   selectorClasses,
   classCreationSelectors,
   initialCards,
-} from "../utils/constants.js";
+} from "./utils/constants.js";
 
 import {
   popupForm,
@@ -18,7 +19,7 @@ import {
   placePopupOpenButton,
   popupPlaceForm,
   buttonPlaceSubmit,
-} from "../utils/domElements.js";
+} from "./utils/domElements.js";
 
 const createCard = (item) => {
   const card = new Card(item, selectorClasses.template, (obj) =>
