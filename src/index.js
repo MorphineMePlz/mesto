c5165d876ad23920c5d48acd3f3349fd35d1ded5
Азиз Гейдар <azizgeidar@Azizs-MacBook-Air.local>
2022-09-09 22:08:28 +0600

    feat: add handleResponse

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Section from "./scripts/Section.js";
 import PopupWithImage from "./scripts/PopupWithImage.js";
 import PopupWithForm from "./scripts/PopupWithForm.js";
 import UserInfo from "./scripts/UserInfo.js";
-import Api from "./scripts/Api.js";
+import { api } from "./scripts/Api.js";
 
 import { selectorClasses, classCreationSelectors } from "./utils/constants.js";
 
@@ -30,28 +30,12 @@ const userInfo = new UserInfo({
   jobSelector: classCreationSelectors.userJob,
 });
 
-const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-50",
-  headers: {
-    authorization: "ecb6ef6c-d4a1-4cc5-86ed-4ee02166ff91",
-    "Content-Type": "application/json",
-  },
-});
-
-const apiCards = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-50/cards",
-  headers: {
-    authorization: "ecb6ef6c-d4a1-4cc5-86ed-4ee02166ff91",
-    "Content-Type": "application/json",
-  },
-});
-
 api
   .getUserInformation()
   .then((res) => {
     userInfo.setUserInfo({ name: res.name, job: res.about });
   })
-  .catch((error) => console.log("Ошибка:"`${error}`));
+  .catch((error) => console.log(error));
 
 const cardsArrayFromServer = [];
 const section = new Section(
@@ -65,12 +49,15 @@ const section = new Section(
   classCreationSelectors.cardList
 );
 
-apiCards.getInitialCards().then((res) => {
-  res.map((element) => {
-    cardsArrayFromServer.push(element);
-  });
-  section.generateCards();
-});
+api
+  .getInitialCards()
+  .then((res) => {
+    res.map((element) => {
+      cardsArrayFromServer.push(element);
+    });
+    section.generateCards();
+  })
+  .catch((error) => console.log(error));
 
 const popupProfile = new PopupWithForm({
   popupSelector: classCreationSelectors.profilePopup,
--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -4,22 +4,32 @@ class Api {
     this._headers = setting.headers;
   }
 
+  handelResponse(res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status} - ${res.statusText}`);
+    }
+    return res.json();
+  }
+
   getUserInformation() {
     return fetch(`${this._address}/users/me`, {
       method: "GET",
       headers: this._headers,
-    }).then((res) => res.json());
+    }).then((res) => this.handelResponse(res));
   }
 
   getInitialCards() {
-    return fetch(this._address, {
+    return fetch(`${this._address}/cards`, {
       method: "GET",
       headers: this._headers,
-    }).then((res) => res.json());
-    //   .then((result) => {
-    //     // console.log(result);
-    //   });
+    }).then((res) => this.handelResponse(res));
   }
 }
 
-export default Api;
+export const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-50",
+  headers: {
+    authorization: "ecb6ef6c-d4a1-4cc5-86ed-4ee02166ff91",
+    "Content-Type": "application/json",
+  },
+});
